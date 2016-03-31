6ec0e4ea171b18fd138bac98f5377e5e35f2116c
Tom Evans <Tom@sawsoftmill.com>
2016-03-31 11:58:50 -0400

    setup wp

---
+++ config/webpack.config.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  entry: '../src.app.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+}
